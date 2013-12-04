c25ba844d84ebe6ed945d2146510adc796da58e5
Yomguithereal <guillaumeplique@gmail.com>
2013-12-04 11:45:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#MTG Parsers
+WIP
---
+++ tests/unit.parser.js
@@ -0,0 +1,17 @@
+/**
+ * MTG Parsers Unit Testing
+ * =========================
+ *
+ * Author: PLIQUE Guillaume (Yomguithereal)
+ */
+
+var assert = require('assert');
+
+/**
+ * .dec file format
+ */
+describe('.dec file format', function() {
+  it('should return true', function() {
+    assert.equal(true, 1 === 1);
+  });
+});
